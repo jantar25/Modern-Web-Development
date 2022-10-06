4008363b4df55e3bce66a49e12e31698c866471b
unknown <glodybulambo@gmail.com>
2022-10-06 19:50:14 +0200

    unicafe step 4 done

--- Part_1/unicafe/src/App.js
+++ Part_1/unicafe/src/App.js
@@ -3,24 +3,46 @@ import { useState } from 'react'
   
 const Button = ({handleClick,text}) =><button onClick={handleClick}>{text}</button>
 
-const Statistics = ({stats,text,sign}) =><div>{text} {stats} {sign}</div>
+const Stat = ({stats,text,sign}) =><div>{text} {stats} {sign}</div>
 
-
+const Statistics = ({allClicks,good,neutral,bad}) => {
+  if (allClicks.length === 0) {
+    return (
+      <div>
+        No feedback given
+      </div>
+    )
+  }
+  return (
+    <div>
+        <Stat stats={good} text='good' sign='' />
+        <Stat stats={neutral} text='neutral' sign='' />
+        <Stat stats={bad} text='bad' sign='' />
+        <Stat stats={allClicks.length} text='all' sign='' />
+        <Stat stats={(good*1+neutral*0+bad*-1)/(allClicks.length)} text='average' sign='' />
+        <Stat stats={(good/(allClicks.length))*100} text='positive' sign='%' /> 
+    </div>
+  )
+}
 
 const App = () => {
   const [good, setGood] = useState(0)
   const [neutral, setNeutral] = useState(0)
   const [bad, setBad] = useState(0)
+  const [allClicks, setAllClicks] = useState([])
 
   const handleGoodClick = () => {
+    setAllClicks(allClicks.concat('g'))
     setGood(good + 1)
   }
 
   const handleNeutralClick = () => {
+    setAllClicks(allClicks.concat('n'))
     setNeutral(neutral + 1)
   }
 
   const handleBadClick = () => {
+    setAllClicks(allClicks.concat('b'))
     setBad(bad + 1)
   }
   
@@ -38,14 +60,7 @@ const App = () => {
       </div>
       <div>
         <h1>statistics</h1>
-        <div>
-          <Statistics stats={good} text='good' sign='' />
-          <Statistics stats={neutral} text='neutral' sign='' />
-          <Statistics stats={bad} text='bad' sign='' />
-          <Statistics stats={bad+neutral+good} text='all' sign='' />
-          <Statistics stats={(good*1+neutral*0+bad*-1)/(bad+neutral+good)} text='average' sign='' />
-          <Statistics stats={(good/(bad+neutral+good))*100} text='positive' sign='%' />
-        </div>
+        <Statistics good={good} bad={bad} neutral={neutral} allClicks={allClicks} />
       </div>
     </div>
   )
