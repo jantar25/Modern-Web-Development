d34fe038da5108c812ed2754928d264145845c3d
unknown <glodybulambo@gmail.com>
2022-10-17 23:32:15 +0200

    Phonebook Backend step 2

--- Part_3/phonebookBackend/index.js
+++ Part_3/phonebookBackend/index.js
@@ -26,13 +26,14 @@ let persons = [
 ]
 
   
-  app.get('/', (request, response) => {
-    response.send('<h1>Hello World!</h1>')
+  app.get('/info', (request, response) => {
+    response.send(`Phonebook has info for ${persons.length} people ${new Date()}`)
   })
   
   app.get('/api/persons', (request, response) => {
     response.json(persons)
   })
+
   
   const PORT = 3001
   app.listen(PORT, () => {
