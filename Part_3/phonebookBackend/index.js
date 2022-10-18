572d236be792a2f7689cbdf78bd4a7bb225278dd
unknown <glodybulambo@gmail.com>
2022-10-18 21:10:31 +0200

    Phonebook Backend step 4

--- Part_3/phonebookBackend/index.js
+++ Part_3/phonebookBackend/index.js
@@ -44,6 +44,13 @@ let persons = [
       response.status(404).end('Not found')
     }
   })
+
+  app.delete('/api/persons/:id', (request,response) => {
+    const id = Number(request.params.id)
+    persons = persons.filter(person => person.id !== id)
+
+    response.status(204).end()
+  })
   
   const PORT = 3001
   app.listen(PORT, () => {
