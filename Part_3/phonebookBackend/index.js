58313f84c538dd29084ade26d96dade9265dfc01
unknown <glodybulambo@gmail.com>
2022-10-26 23:08:54 +0200

    phonebook database step1&2 done

--- Part_3/phonebookBackend/index.js
+++ Part_3/phonebookBackend/index.js
@@ -1,7 +1,11 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
+
+const Person = require('./models/persons')
 const app = express()
+
 
 app.use(cors())
 app.use(express.json())
@@ -9,44 +13,24 @@ morgan.token('body', (req, res) => JSON.stringify(req.body));
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
 app.use(express.static('build'))
 
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
 
 
 //GET INFORMATION
   app.get('/info', (request, response) => {
-    response.send(`Phonebook has info for ${persons.length} people ${new Date()}`)
+    response.send(`Phonebook has info for people `)
   })
   
 //GET ALL PERSONS
   app.get('/api/persons', (request, response) => {
-    response.json(persons)
+    Person.find({}).then(persons => {
+      response.json(persons)
+    })
   })
 
 //GET ONE PERSON BY ID
   app.get('/api/persons/:id',(request,response) => {
     const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
+    const person = Person.find(person => person.id === id)
   
     if (person) {
       response.json(person)
@@ -65,30 +49,33 @@ let persons = [
 
 //CREATE ONE PERSON
   app.post('/api/persons', (request, response) => {
-    const id = Math.floor(Math.random() * 1000)
     const body = request.body
-    const existingName = persons.find(person => person.name === body.name)
+    const existingName = Person.findOne({name:body.name})
+    console.log(existingName)
     if (!body.name || !body.number) {
       return response.status(400).json({ 
         error: 'content missing' 
       })
-    } else if (existingName) {
-      return response.status(400).json({ 
-        error: 'name must be unique'
-       })
-    } else {
-      const person = {
-        id: id,
+    }
+    // else if (existingName) {
+    //   return response.status(400).json({ 
+    //     error: 'name must be unique'
+    //    })
+    // } 
+    else {
+      const person = new Person({
         name: body.name,
-        number: body.number
-      }
+        number: body.number,
+        date: new Date(),
+      })
     
-      persons = persons.concat(person)
-      response.json(person)
+      person.save().then(savedPerson =>{
+        response.json(savedPerson)
+      })   
     }
   })
   
-  const PORT = process.env.PORT || 3001
+  const PORT = process.env.PORT
   app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
   })
