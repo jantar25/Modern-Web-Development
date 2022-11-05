785229f218aad1213b46b86d82268bad3604cd59
unknown <glodybulambo@gmail.com>
2022-11-05 17:38:11 +0200

    phonebook database step8 working

--- Part_3/phonebookBackend/models/persons.js
+++ Part_3/phonebookBackend/models/persons.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 const url = process.env.MONGODB_URI
 const numberValidator = (v) => {
-  return /\d{2,3}-\d{6,}$/.test(v)
+  return /^\d{2,3}-\d{6,}$/.test(v)
 }
 
 mongoose.connect(url).then(() => {
