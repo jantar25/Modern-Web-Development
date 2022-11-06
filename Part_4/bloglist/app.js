54f9f5f95cbf576a23e300ef7b136128de587a72
unknown <glodybulambo@gmail.com>
2022-11-06 19:43:21 +0200

    bloglist step2 done

---
+++ Part_4/bloglist/app.js
@@ -0,0 +1,28 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const mongoose = require('mongoose')
+const { MONGODB_URI } = require('./utils/config')
+const { info,error } = require('./utils/logger')
+const blogsRouter = require('./controllers/blogs')
+const middleware = require('./utils/middleware')
+
+mongoose.connect(MONGODB_URI)
+  .then(() => {
+    info('connected to MongoDB')
+  })
+  .catch((error) => {
+    error('error connecting to MongoDB:', error.message)
+  })
+
+app.use(cors())
+app.use(express.json())
+app.use(middleware.requestLogger)
+app.use(middleware.morganMiddleware(':method :url :status :res[content-length] - :response-time ms :body '))
+
+app.use('/api/blogs',blogsRouter)
+
+app.use(middleware.unknownEndpoint)
+app.use(middleware.errorHandler)
+
+module.exports = app
--- Part_4/bloglist/index.js
+++ Part_4/bloglist/index.js
@@ -1,59 +1,12 @@
+
+const app = require('./app')
 const http = require('http')
-const express = require('express')
-const app = express()
-const cors = require('cors')
-const mongoose = require('mongoose')
-require('dotenv').config()
-const morgan = require('morgan')
+const { PORT } = require('./utils/config')
+const { info } = require('./utils/logger')
 
-const blogSchema = new mongoose.Schema({
-  title: String,
-  author: String,
-  url: String,
-  likes: Number
-})
+const server = http.createServer(app)
 
-blogSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-})
-const Blog = mongoose.model('Blog', blogSchema)
 
-const mongoUrl = process.env.MONGODB_URI
-mongoose.connect(mongoUrl).then(() => {
-    console.log('connected to MongoDB')
-  })
-    .catch((error) => {
-      console.log('error connecting to MongoDB:', error.message)
-    })
-
-app.use(cors())
-app.use(express.json())
-morgan.token('body', (req) => JSON.stringify(req.body))
-app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body '))
-
-app.get('/api/blogs', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
-
-app.post('/api/blogs', (request, response) => {
-  const blog = new Blog(request.body)
-
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-})
-
-const PORT = process.env.PORT || 3003
-app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`)
+server.listen(PORT, () => {
+  info(`Server running on port ${ PORT }`)
 })
