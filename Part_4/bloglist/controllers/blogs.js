244312b90b052f7adfcb4aad70647f3996e40cf3
unknown <glodybulambo@gmail.com>
2022-11-14 04:40:00 +0200

    blog list expensions step 1&2

--- Part_4/bloglist/controllers/blogs.js
+++ Part_4/bloglist/controllers/blogs.js
@@ -1,12 +1,13 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
 
-
+//GET ALL BLOGS
 blogsRouter.get('/', async (request, response) => {
   const blogs = await Blog.find({})
   response.json(blogs)
 })
 
+//ADD ONE BLOG
 blogsRouter.post('/', async (request, response) => {
   const body = request.body
   if (!body.title || !body.author) {
@@ -19,5 +20,23 @@ blogsRouter.post('/', async (request, response) => {
     response.status(201).json(savedBlog)
   }
 })
+
+//DELETE ONE BLOG BY ID
+blogsRouter.delete('/:id', async (request, response) => {
+  await Blog.findByIdAndRemove(request.params.id)
+  response.status(204).end()
+})
+
+//UPDATE EXISTING BLOG
+blogsRouter.put('/:id', async (request, response) => {
+  const body = request.body
+  const blog = {
+    likes: body.likes,
+  }
+  const updatedBlog = await Blog.findByIdAndUpdate(request.params.id, blog,
+    { new: true, runValidators: true, context: 'query' })
+  response.status(200).json(updatedBlog)
+})
+
 
 module.exports = blogsRouter
--- Part_4/bloglist/tests/blog_api.test.js
+++ Part_4/bloglist/tests/blog_api.test.js
@@ -15,70 +15,108 @@ beforeEach(async () => {
 },100000)
 
 
+describe('when there is initially some blogs saved', () => {
+  test('blogs are returned as json', async () => {
+    await api
+      .get('/api/blogs')
+      .expect(200)
+      .expect('Content-Type', /application\/json/)
+  })
 
-test('blogs are returned as json', async () => {
-  await api
-    .get('/api/blogs')
-    .expect(200)
-    .expect('Content-Type', /application\/json/)
+
+  test('all blogs are returned', async () => {
+    const response = await api.get('/api/blogs')
+    expect(response.body).toHaveLength(initialBlogs.length)
+  })
+
+  test('verifies unique identifier of blog posts is named id', async () => {
+    const response = await api.get('/api/blogs')
+    const contents = response.body.map(r => r.id)
+    expect(contents[0]).toBeDefined()
+  })
 })
 
+describe('addition of a new blog', () => {
+  test('A valid content blog can be added ', async () => {
+    const newBlog = {
+      title: 'Hashlem Whitchwes',
+      author: 'Ed W. Doe',
+      url: 'http://www.u.arizona.edu/~rubinson/hashlemWhitches',
+      likes: 8,
+    }
 
-test('all blogs are returned', async () => {
-  const response = await api.get('/api/blogs')
-  expect(response.body).toHaveLength(initialBlogs.length)
+    await api
+      .post('/api/blogs')
+      .send(newBlog)
+      .expect(201)
+      .expect('Content-Type', /application\/json/)
+
+    const blogsAtEnd = await blogInDb()
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1)
+
+    const contents = blogsAtEnd.map(b => b.title)
+    expect(contents).toContain(
+      'Hashlem Whitchwes'
+    )
+  })
+
+  test('blog without content is not added', async () => {
+    const blog = {
+      url: 'http://www.u.arizona.edu/~rubinson/Pinchards',
+    }
+
+    await api
+      .post('/api/blogs')
+      .send(blog)
+      .expect(400)
+
+    const blogsAtEnd = await blogInDb()
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length)
+  })
+})
+describe('viewing a specific blog', () => {
+  test('missing blog likes return 0 value', () => {
+    const blog = {
+      title: 'Pinchards',
+      author: 'Ed W. Doe',
+      url: 'http://www.u.arizona.edu/~rubinson/Pinchards',
+    }
+    expect(missingLike(blog)).toBe(0)
+  })
 })
 
-test('verifies unique identifier of blog posts is named id', async () => {
-  const response = await api.get('/api/blogs')
-  const contents = response.body.map(r => r.id)
-  expect(contents[0]).toBeDefined()
+describe('deletion of a blog', () => {
+  test('blog deletion with valid id', async () => {
+    const blogsAtStart = await blogInDb()
+    const blogToDelete = blogsAtStart[0]
+
+    await api
+      .delete(`/api/blogs/${blogToDelete.id}`)
+      .expect(204)
+
+    const blogsAtEnd = await blogInDb()
+    expect(blogsAtEnd).toHaveLength(initialBlogs.length - 1)
+  })
 })
 
-test('a valid blog can be added ', async () => {
-  const newBlog = {
-    title: 'Hashlem Whitchwes',
-    author: 'Ed W. Doe',
-    url: 'http://www.u.arizona.edu/~rubinson/hashlemWhitches',
-    likes: 8,
-  }
+describe('Updating a blog', () => {
+  test('blog update with valid id', async () => {
+    const blogsAtStart = await blogInDb()
+    const blogToupdate = blogsAtStart[0]
 
-  await api
-    .post('/api/blogs')
-    .send(newBlog)
-    .expect(201)
-    .expect('Content-Type', /application\/json/)
+    const blog = {
+      likes: 39
+    }
 
-  const blogsAtEnd = await blogInDb()
-  expect(blogsAtEnd).toHaveLength(initialBlogs.length + 1)
+    await api
+      .put(`/api/blogs/${blogToupdate.id}`)
+      .send(blog)
+      .expect(200)
 
-  const contents = blogsAtEnd.map(b => b.title)
-  expect(contents).toContain(
-    'Hashlem Whitchwes'
-  )
-})
-
-test('missing blog likes return 0 value', () => {
-  const blog = {
-    title: 'Pinchards',
-    author: 'Ed W. Doe',
-    url: 'http://www.u.arizona.edu/~rubinson/Pinchards',
-  }
-  expect(missingLike(blog)).toBe(0)
-})
-
-test('blog without content is not added', async () => {
-  const blog = {
-    url: 'http://www.u.arizona.edu/~rubinson/Pinchards',
-  }
-
-  await api
-    .post('/api/blogs')
-    .send(blog)
-    .expect(400)
-
-  const notesAtEnd = await blogInDb()
-  expect(notesAtEnd).toHaveLength(initialBlogs.length)
+    const blogsAtEnd = await blogInDb()
+    const contents = blogsAtEnd.map(b => b.likes)
+    expect(contents).toContain(39)
+  })
 })
 
 afterAll(() => {
