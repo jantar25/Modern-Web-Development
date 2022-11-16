abb0fa4f20196c4272eabec481e21a978a033cfb
unknown <glodybulambo@gmail.com>
2022-11-17 00:39:21 +0200

    blog list expensions step 5

--- Part_4/bloglist/controllers/blogs.js
+++ Part_4/bloglist/controllers/blogs.js
@@ -1,9 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blogs')
+const User = require('../models/user')
 
 //GET ALL BLOGS
 blogsRouter.get('/', async (request, response) => {
-  const blogs = await Blog.find({})
+  const blogs = await Blog.find({}).populate('user', { username: 1, name: 1 })
   response.json(blogs)
 })
 
@@ -15,8 +16,17 @@ blogsRouter.post('/', async (request, response) => {
       error: 'content missing'
     })
   } else {
-    const blog = new Blog(body)
+    const user = await User.findById(body.userId)
+    const blog = new Blog({
+      title: body.title,
+      author: body.author,
+      url: body.url,
+      likes: body.likes,
+      user: user._id
+    })
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+    await user.save()
     response.status(201).json(savedBlog)
   }
 })
