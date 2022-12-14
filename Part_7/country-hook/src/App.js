f818a7a0cb150dc343417da47ed83746d95a4a48
unknown <glodybulambo@gmail.com>
2022-12-14 13:33:17 +0200

    country hook refactoring

--- Part_7/country-hook/src/App.js
+++ Part_7/country-hook/src/App.js
@@ -1,47 +1,6 @@
-import { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const useField = (type) => {
-  const [value, setValue] = useState('')
-
-  const onChange = (event) => {
-    setValue(event.target.value)
-  }
-  return {
-    type,
-    value,
-    onChange
-  }
-}
-
-const useCountry = (name) => {
-  const [country, setCountry] = useState(null)
-  console.log(country)
-  useEffect(() => {
-    const getCountry = async (name) =>{
-      const country = await axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
-      setCountry(country.data[0])
-    }
-    getCountry(name)
-  },[name])
-
-  return country
-}
-
-const Country = ({ country }) => {
-  if (!country) {
-    return <div>not found...</div>
-  }
-
-  return (
-    <div>
-      <h3>{country.name.common}</h3>
-      <div>population {country.population}</div> 
-      <div>capital {country.capital}</div>
-      <img src={country.flags.png} height='100' alt={`flag of ${country.name.common}`}/> 
-    </div>
-  )  
-}
+import { useState } from 'react'
+import { useCountry,useField } from './hooks'
+import Country from './components/Country'
 
 const App = () => {
   const nameInput = useField('text')
--- Part_7/country-hook/src/hooks/index.js
+++ Part_7/country-hook/src/hooks/index.js
@@ -1,10 +1,29 @@
-import { useEffect } from 'react'
+import { useState,useEffect } from 'react'
 import axios from 'axios'
 
 export const useCountry = (name) => {
-    console.log(name)
+    const [country, setCountry] = useState(null)
+
     useEffect(() => {
-        const country = axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
-        return country.data
+      const getCountry = async (name) =>{
+        const country = await axios.get(`https://restcountries.com/v3.1/name/${name}?fullText=true`)
+        setCountry(country.data[0])
+      }
+      getCountry(name)
     },[name])
+  
+    return country
+  }
+
+export const useField = (type) => {
+  const [value, setValue] = useState('')
+  
+  const onChange = (event) => {
+    setValue(event.target.value)
+  }
+  return {
+    type,
+    value,
+    onChange
+  }
 }
