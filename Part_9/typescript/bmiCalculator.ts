3951cbda5078ed8d7fb640d94209864f0aba5238
unknown <glodybulambo@gmail.com>
2023-01-05 23:20:05 +0200

    BMI Calculator done

--- Part_9/typescript/bmiCalculator.ts
+++ Part_9/typescript/bmiCalculator.ts
@@ -1,15 +1,23 @@
 
-const calculateBmi = (height : number,weight : number): number => {
-const result = weight/((height*height))
-// if (result < 18.5) {
-//     return 'underweight (small weight)';
-//   } else if (18.5 <= result && result <24.9) {
-//     return 'Normal (healthy weight)';
-//   } else if (25 <= result && result < 29.9) {
-//     return 'overweight (big weight)';
-//   }
-// return 'obese'
-return result
+const calculateBmi = (height : number,weight : number): string => {
+const result = weight/((height*0.01)**2)
+// switch(result) {
+//     case (result < 18.5) {
+
+//     }
+// }
+console.log(result)
+if (result < 18.5) {
+    return 'underweight (small weight)';
+  } else if ( 18.5 < result && result < 24.9) {
+    return 'Normal (healthy weight)';
+  } else if ( 25 <result && result < 29.9) {
+    return 'overweight (big weight)';
+  } else if ( result > 30) {
+    return 'obese';
+  } else {
+    return 'Incorect input'
+  }
 }
 
-console.log(calculateBmi(180, 74))
+console.log(calculateBmi(160, 34))
