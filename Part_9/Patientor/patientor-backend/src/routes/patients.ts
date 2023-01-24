a92e4b1bcd3b9015266c6ba63d9f1d542f773396
unknown <glodybulambo@gmail.com>
2023-01-24 22:40:20 +0200

    patientor, step8 ongoing

--- Part_9/Patientor/patientor-backend/src/routes/patients.ts
+++ Part_9/Patientor/patientor-backend/src/routes/patients.ts
@@ -38,4 +38,22 @@ router.post('/', (req, res) => {
   }  
 });
 
+router.post('/:id/entries',(req,res) => {
+  const id = req.params.id;
+  console.log(id);
+  console.log(req.body);
+  try {
+    const Patient = patientsServices.getPatient(id);
+    // const entry = Patient.entries.concat(req.body);
+    res.json(Patient);
+    // console.log(entry);
+  } catch (error: unknown) {
+    let errorMessage = 'Something went wrong.';
+    if (error instanceof Error) {
+      errorMessage += ' Error: ' + error.message;
+    }
+    res.status(400).send(errorMessage);
+  } 
+});
+
 export default router;
