a1a69ea1537ffa44ad5531f0afc5ecffeb0a1c44
unknown <glodybulambo@gmail.com>
2023-02-11 03:48:59 +0200

    Books by genre

--- Part_8/BookGraphicQL/index.js
+++ Part_8/BookGraphicQL/index.js
@@ -99,7 +99,7 @@ let authors = [
   type Query {
     bookCount: Int!
     authorCount:Int!
-    allBooks(author: String!): [Book]
+    allBooks(genre: String!): [Book]
     allAuthors:[Author!]!
   }
 `
@@ -108,7 +108,9 @@ const resolvers = {
   Query: {
     bookCount: () => books.length,
     authorCount: () => authors.length,
-    allBooks: (root, args) => books.filter(book => book.author === args.author),
+    allBooks: (root, args) => books.filter(book => 
+      book.genres.includes(args.genre)
+      ),
     allAuthors: () => authors.map(author => ({
       ...author,
       bookCount:(books.filter(book =>
