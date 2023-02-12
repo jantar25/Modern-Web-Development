d60a762686ec9012dc367a4f3024a7834a63bcea
unknown <glodybulambo@gmail.com>
2023-02-12 02:25:39 +0200

    project set up

---
+++ Part_8/library-frontend/src/App.js
@@ -0,0 +1,33 @@
+import {
+  BrowserRouter as Router,
+  Routes, Route, Link
+} from 'react-router-dom'
+import Authors from './components/Authors'
+import Books from './components/Books'
+import NewBook from './components/NewBook'
+
+const App = () => {
+  return (
+    <Router>
+      <div>
+        <button>
+          <Link style={{textDecoration:'none'}} to="/">authors</Link>
+        </button>
+        <button>
+          <Link style={{textDecoration:'none'}} to="/books">books</Link>
+        </button>
+        <button>
+          <Link style={{textDecoration:'none'}} to="/addBook">add book</Link>
+        </button>
+      </div>
+
+      <Routes>
+        <Route path="/" element={<Authors />} />
+        <Route path="/books" element={<Books />} />
+        <Route path="/addBook" element={<NewBook />} />
+      </Routes>
+    </Router>
+  )
+}
+
+export default App
